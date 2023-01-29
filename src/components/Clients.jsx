f752724b18cde8e17c84eea35de35a69d83c3774
Thế Hiếu <thehieu236@gmail.com>
2023-01-30 00:30:24 +0700

    Testimonials section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import styles from "../constants/style.js";
+import {clients} from "../constants/index.js";
 
 const Clients = () => {
     return (
-        <div>
+        <section className={`${styles.flexCenter} my-4`}>
+            <div className={`${styles.flexCenter} flex-wrap w-full`}>{
+                clients.map((client) => (
+                    <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[128px]`}>
+                        <img src={client.logo} alt="client" className={"sm:w-[192px] w-[100px] object-contain"}/>
+                    </div>
 
-        </div>
+                ))
+            }
+
+            </div>
+
+        </section>
     );
 };
 
