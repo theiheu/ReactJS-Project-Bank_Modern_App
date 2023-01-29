31a01308815395ad009f5e7a12be4b6eec7232fb
Thế Hiếu <thehieu236@gmail.com>
2023-01-29 23:31:30 +0700

    CarDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import {card} from "../assets/index.js";
+import styles, {layout} from "../constants/style.js";
+import Button from "./Button.jsx";
 
 const CardDeal = () => {
     return (
-        <div>
+        <section className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>Find a better card deal <br className={"sm:block hidden"}/> in few easy
+                    steps.</h2>
+                <p className={`${styles.paragraph}`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+                    aliquet eget maurids torotor.
+                    Aliquet ultrices ac, amtau.</p>
+                <Button styles={"mt-10"}/>
+            </div>
+            <div className={layout.sectionImg}>
+                <img src={card} alt="card" className={"w-[100%] h-[100%]"}/>
+            </div>
 
-        </div>
+        </section>
     );
 };
 
